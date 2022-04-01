c747469bcbc6cbce8961bbebebfc72f7fa7a86a4
dexon03 <89253308+dexon03@users.noreply.github.com>
2022-04-01 11:06:04 +0300

    Add files via upload

---
+++ Lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using static Lab2dotnet.functions;
+
+namespace Lab2dotnet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string[]> listOfCLients = InputText();
+            string path = "file1.dat";
+            CreateAndWriteFile(path,listOfCLients);
+            string textFromFirstFile = ReadFile(path);
+            Console.WriteLine("\nТекст з першого файлу:");
+            Console.WriteLine(textFromFirstFile);
+            List<string[]> resultList = FindSpecialClients(listOfCLients);
+            string path2 = "resultFile.dat";
+            CreateAndWriteFile(path2,resultList);
+            Console.WriteLine("\nТекст з другого файлу:");
+            string textFromResultFile = ReadFile(path2);
+            Console.WriteLine(textFromResultFile);
+
+        }
+    }
+}
---
+++ Lab2/functions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab2dotnet
+{
+    public static class functions
+    {
+        public static List<string[]> InputText()
+        {
+            List<string[]> list = new List<string[]>();
+            Console.WriteLine("Якшо хочете закінчити ввід - натисність delete, інакше любу іншу клавішу.");
+            do
+            {
+                Console.Write("Введіть прізвище:");
+                var a = Console.ReadKey();
+                if (a.Key == ConsoleKey.Delete) break;
+                
+                string[] person = new string[3];
+                
+                person[0] = a.KeyChar + Console.ReadLine();
+                do
+                {
+                    Console.Write("Введіть час приходу в форматі (HH:mm):");
+                    do
+                    {
+                        person[1] = Console.ReadLine();
+                        if (ValidTime(person[1])) break;
+                        
+
+                        Console.Write("Ви ввели час в неправильному форматі, або недопустипі значення. Будь ласка введіть коректний час:");
+                    } while (true);
+                
+                    Console.Write("Введіть час закінчення обслуговування в форматі (HH:mm):");
+                    do
+                    {
+                        person[2] = Console.ReadLine();
+                        if (ValidTime(person[2]) && isAdmissible(person[1],person[2])) break;
+
+                        Console.Write("Ви ввели час в неправильному форматі, або недопустипі значення. Будь ласка введіть коректний час:");
+                    } while (true);
+
+                    if (!isCrossing(list, person[1], person[2])) break;
+                    
+
+                    Console.WriteLine("Час прийому який ви ввели неможливий, оскільки пересікаєтся з вже існуючими клієнтами, спробуйте ще раз.");
+                } while (true);
+                
+
+                list.Add(person);
+            } while (true);
+
+            return list;
+        }
+
+        public static bool isAdmissible(string time1, string time2)
+        {
+            DateTime time_start = DateTime.Parse(time1);
+            DateTime time_end = DateTime.Parse(time2);
+            if (time_end < time_start) return false;
+            return true;
+        }
+
+        public static bool ValidTime(string time)
+        {
+            Regex regex = new Regex(@"\d{2}:\d{2}");
+            if (regex.IsMatch(time))
+            {
+                string[] digits = time.Split(new[] {':'});
+                if (Int32.Parse(digits[0]) <= 18 && Int32.Parse(digits[0]) >= 9 && Int32.Parse(digits[1]) < 60 &&
+                    Int32.Parse(digits[0]) > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool isCrossing(List<string[]> list, string time_start, string time_end)
+        {
+            if (list.Count != 0)
+            {
+                DateTime date_start1 = DateTime.Parse(time_start);
+                DateTime date_end1 = DateTime.Parse(time_end);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    DateTime date_start2 = DateTime.Parse(list[i][1]);
+                    DateTime date_end2 = DateTime.Parse(list[i][2]);
+                    if (date_start1 > date_start2 && date_start1 < date_end2 ||
+                        date_end1 > date_start2 && date_end1 < date_end2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+
+            return false;
+        }
+
+        public static void CreateAndWriteFile(string path,List<string[]> list)
+        {
+            
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                string line = null;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    line += (line==null?"":"\n")+  $"Прізвище:{list[i][0]}\t Час приходу: {list[i][1]}\t Час закінчення:{list[i][2]}";
+                    
+                }
+                writer.Write(line);
+            }
+
+        }
+
+        public static string ReadFile(string path)
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(path,FileMode.Open)))
+            {
+                string text = reader.ReadString();
+                return text;
+            }
+        }
+
+        public static List<string[]> FindSpecialClients(List<string[]> list)
+        {
+            List<string[]> resultList = new List<string[]>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                DateTime time1 = DateTime.Parse(list[i][1]);
+                DateTime time2 = DateTime.Parse(list[i][2]);
+                if ((time2 - time1).Minutes > 30)
+                {
+                    resultList.Add(list[i]);
+                }
+            }
+
+            return resultList;
+        }
+
+
+    }
+}
