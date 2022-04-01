5183fcd8c4f584d101a4e4cb96768d01c219ec2c
dexon03 <89253308+dexon03@users.noreply.github.com>
2022-04-01 19:43:07 +0300

    Update Program.cs

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using static Lab2dotnet.functions;
@@ -10,18 +10,22 @@ namespace Lab2dotnet
         public static void Main(string[] args)
         {
             List<string[]> listOfCLients = InputText();
-            string path = "file1.dat";
+            string path = "file1.dat";a
             CreateAndWriteFile(path,listOfCLients);
             string textFromFirstFile = ReadFile(path);
             Console.WriteLine("\nТекст з першого файлу:");
             Console.WriteLine(textFromFirstFile);
             List<string[]> resultList = FindSpecialClients(listOfCLients);
-            string path2 = "resultFile.dat";
-            CreateAndWriteFile(path2,resultList);
-            Console.WriteLine("\nТекст з другого файлу:");
-            string textFromResultFile = ReadFile(path2);
-            Console.WriteLine(textFromResultFile);
-
+            if (resultList.Count != 0)
+            {
+                string path2 = "resultFile.dat";
+                CreateAndWriteFile(path2,resultList);
+                Console.WriteLine("\nТекст з другого файлу:");
+                string textFromResultFile = ReadFile(path2);
+                Console.WriteLine(textFromResultFile);
+            }
+            else Console.WriteLine("\nНемає клієнтів з тривалістю прийому від 30 хвилин.");
+            
         }
     }
 }
