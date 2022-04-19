ee9d9c62f7d5b57dc7062f15da3c2c4ee7c32abb
dexon03 <dima0958757803@gmail.com>
2022-04-19 16:31:03 +0200

    lab4

--- Lab4/ConsoleApplication1/Functions.cs
+++ Lab4/ConsoleApplication1/Functions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.RegularExpressions;
-using static System.Text.RegularExpressions.Regex;
 
 namespace ConsoleApplication1
 {
@@ -7,9 +6,7 @@ namespace ConsoleApplication1
     {
         public static bool isValid(string date)
         {
-            // Regex regex1 = new Regex(@"^\d{4}(-|\/|\.)(0?[1-9]|1[012])\1(0[1-9]|[12][0-9]|3[01])(\s*)");
             Regex regex2 = new Regex(@"^(0[1-9]|[12][0-9]|3[01])\.(0?[1-9]|1[012])\.\d{4}(\s*)");
-            // Regex regex3 = new Regex(@"^(0?[1-9]|1[012])(-|\/|\.)(0[1-9]|[12][0-9]|3[01])(\s*)");
             Regex regex4 = new Regex(@"^(0[1-9]|[12][0-9]|3[01])\.(0?[1-9]|1[012])(\s*)");
 
             if ( regex2.IsMatch(date) || regex4.IsMatch(date)) return true;
